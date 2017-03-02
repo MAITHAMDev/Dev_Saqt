a4bda9f27fd17cab65d58071e24d3a865074d0bd
ii02ii <abolove4@yahoo.com>
2017-03-02 21:46:52 +0300

    Update delete.lua

--- plugins/delete.lua
+++ plugins/delete.lua
@@ -1,27 +1,22 @@
---[[
-# For More Information ....! 
-# Developer : Aziz < @devss_bot >      #Dev
-# our channel: @help_tele
-]]
-local function history(extra, suc, result)
+local function Ch_Dev(extra, suc, result)
   for i=1, #result do
     delete_msg(result[i].id, ok_cb, false)
   end
   if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..' 🚭تہم مہسہح نہفآيہآتہ آلہكہروبہ ويہمہنہع آلہتہدخہيہن دآخہل آلہكہروبہ 😹🙌"', ok_cb, false)
+    send_msg(extra.chatid, '('..#result..')  رسہۧآلہۧة داخہۧل آلہۧكہۧروبہۧ 🐸🍷 تہۧم  مہۧسہۧحہۧهہۧآ وتہۧعہبہۧتونہۧيے 🌝😹', ok_cb, false)
   else
-    send_msg(extra.chatid, ' عہٰيہٰع شہٰكہٰد وسہٰخہٰيہٰن 😐 تہٰم مہٰسہٰح آلہٰزبہٰل دآخہٰل آلہٰكہٰروب 👍🏻🤖 آنہٰتہٰظہٰروآ آلہٰبہٰلآنہٰكلآدشہٰ يہٰشہٰيہٰلہٰو آلہٰزبہٰل خہٰلہٰيہٰتہٰهہٰآ خہٰآرج آلہٰكہٰروب 🚷🚭', ok_cb, false)
+    send_msg(extra.chatid, 'تہۧم 🐸🍷 مہۧسہۧح الہۧحہۧد آلہۧنہۧهہۧآئہۧيے  آلہۧيے مہۧن آلہۧرسہۧآئہۧل 🌝😹', ok_cb, false)
   end
 end
 local function Memo(msg, matches)
-  if matches[1] == 'امسح' and is_momod(msg) then
+  if matches[1] == 'مسح' and is_momod(msg) then
     if msg.to.type == 'channel' then
-      if tonumber(matches[2]) > 1000000 or tonumber(matches[2]) < 1 then
-        return "only from num 1 to 1000000 !!"
+      if tonumber(matches[2]) > 1000 or tonumber(matches[2]) < 1 then
+        return " عہۧدد آلہۧرسہۧآئہۧل الہۧتہۧيے بہۧآمہۧكہۧآنہۧيے  مہۧسہۧحہۧهہۧآ مہۧن 100-1    🌝🍷"
       end
-      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
+      get_history(msg.to.peer_id, matches[2] + 1 , Ch_Dev , {chatid = msg.to.peer_id, con = matches[2]})
     else
-      return "4 sudo only"
+      return "4 مطورين فقط ؟"
     end
   else
     return 
@@ -30,8 +25,7 @@ end
 
 return {
     patterns = {
-        '^(امسح) (%d*)$',
-        '^(rm) (%d*)$',
+        '^(مسح) (%d*)$',
     },
     run = Memo
 }
